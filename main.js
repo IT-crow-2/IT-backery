9fb0a33acb0663f2863f6f79aeb8ec1b0d211622
LucasManuelRodriguez <rodriguez.m.lucas@gmail.com>
2021-10-21 20:58:14 -0300

    modification of product list in html, adding them from javascript

--- main.js
+++ main.js
@@ -1,37 +1,47 @@
 let listaPanes = [
     {
-        img:"/assets/img-Cards/pan222.png.png",
+        img:"/assets/img-Cards/pan222.png",
         title:"PAN DE CAMPO",
-        precio:"€6,00",
+        precio:"€2,60",
     },
     {
-        img:"/assets/img-Cards/pan222.png.png",
-        title:"PAN DE CAMPO",
-        precio:"€6,00",
+        img:"/assets/img-Cards/croissants.png",
+        title:"CROISSANTS",
+        precio:"€1,20",
     },
     {
-        img:"/assets/img-Cards/pan222.png.png",
+        img:"/assets/img-Cards/pan.png",
         title:"PAN DE CAMPO",
-        precio:"€6,00",
+        precio:"€2,60",
     },
     {
-        img:"/assets/img-Cards/pan222.png.png",
+        img:"/assets/img-Cards/rodajas.png",
         title:"PAN DE CAMPO",
-        precio:"€6,00",
+        precio:"€2,60",
     },
     {
-        img:"/assets/img-Cards/pan222.png.png",
+        img:"/assets/img-Cards/pbt.png",
         title:"PAN DE CAMPO",
-        precio:"€6,00",
+        precio:"€2,60",
     },
     {
-        img:"/assets/img-Cards/pan222.png.png",
+        img:"/assets/img-Cards/tartachoco.png",
         title:"PAN DE CAMPO",
-        precio:"€6,00",
-    }
+        precio:"€2,60",
+    },
+    {
+        img:"/assets/img-Cards/brownie.png",
+        title:"PAN DE CAMPO",
+        precio:"€2,60",
+    },
+    {
+        img:"/assets/img-Cards/cheesecake.png",
+        title:"PAN DE CAMPO",
+        precio:"€2,60",
+    },
 ]
 
-let boxcard = document.getElementById("boxcard")
+let boxcard = document.getElementById("main-mobile2")
 
 let sectioninicial = document.getElementById("sectionInitial")
 
@@ -44,23 +54,17 @@ function arrayread(array){
 }
 
 function printcard(element){
-    boxcard.innerHTML += `<section class="cards-main">
-                                    <div class="card">
+    boxcard.innerHTML +=    `<section class="cards-main">
+                                    <button class="card">
                                         <div class="card-body">
-                                        <img src="${element.img}" alt="">
-                                        <h5 class="card-title">${element.title}</h5>
-                                        <p class="card-text">${element.precio}</p>
-                                    </div>
-                                    <button class="CardButton">ADD</button>
-                                </div>
+                                            <img class="imgCards" src="${element.img}" alt="">
+                                            <h5 class="card-title">${element.title}</h5>
+                                            <p class="card-text"> ${element.precio}</p>
+                                        </div>
+                                        <button class="CardButton">ADD</button>
+                                    </button>
                             </section>`
 }
 
-function openboxmain(){
+arrayread(listaPanes)
     
-    sectioninicial.classList.add("displaynone")
-    boxcard.classList.add("displayon")
-    arrayread(listaPanes)
-    
-}
-
